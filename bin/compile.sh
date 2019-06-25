1d56ad21acd2a761682f78a1dacd0fbd0b214bd0
Peter Bjorklund <piot@hotmail.com>
2019-06-25 16:17:38 +0200

    chore: compile script works

--- bin/compile.sh
+++ bin/compile.sh
@@ -1,1 +1,1 @@
-clang ../src/lib/*.c ../src/examples/repl/*.c ../deps/imprint/*.c ../deps/breathe/*.c  ../deps/tiny-libc/*.c -I ../deps/ -I ../src/include/ -o repl
+clang ../src/lib/*.c ../src/examples/repl/*.c ../deps/imprint/src/lib/*.c ../deps/breathe/src/platform/sdl/*.c  ../deps/tiny-libc/src/lib/*.c -I ../deps/include -I ../src/include/ -o repl -lSDL2
